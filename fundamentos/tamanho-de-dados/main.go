8f7ed98457ccdafd187ff7ce183e1ad0c4bf5752
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-12 23:15:16 -0300

    Tamanho dos tipos de dados

---
+++ fundamentos/tamanho-de-dados/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	var i int
+	var f float64
+	var s string
+	var b bool
+
+	fmt.Printf("Tamanho de 'i' (int): %d bytes\n", unsafe.Sizeof(i))
+	fmt.Printf("Tamanho de 'f' (float64): %d bytes\n", unsafe.Sizeof(f))
+	fmt.Printf("Tamanho de 's' (string): %d bytes\n", unsafe.Sizeof(s))
+	fmt.Printf("Tamanho de 'b' (bool): %d bytes\n", unsafe.Sizeof(b))
+}
