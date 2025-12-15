41d2e40c8694414186c2c9dce972f586bdf396ca
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-15 09:06:26 -0300

    Delete

--- slices-arrays/range/main.go
+++ slices-arrays/range/main.go
@@ -7,6 +7,8 @@ func main() {
 	m := make(map[string]int)
 	m["sp"] = 10000000
 	m["cg"] = 900000
+	m["rj"] = 6000000
+	delete(m, "rj")
 
 	for chave, valor := range m {
 		fmt.Println("Cidade", chave, "H:", valor)
