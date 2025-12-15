8ea6622c3c6580067147d3c6f49079e3c7f81359
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-15 09:01:32 -0300

    Maps na prática

---
+++ slices-arrays/maps-na-pratica/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+	m["sp"] = 10000000
+	m["cg"] = 900000
+
+	valor, existe := m["rj"]
+	if existe {
+		fmt.Println(valor)
+	} else {
+		fmt.Println("chave não existe")
+	}
+
+	fmt.Println(m)
+}
