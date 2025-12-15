e2e9b239b1375190d11d5d0742a6b1f4d9486737
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-15 14:55:35 -0300

    Encapsulamento

--- struct/main.go
+++ struct/main.go
@@ -1,25 +1,20 @@
 package main
 
-import "fmt"
-
-type endereco struct {
-	rua    string
-	numero int
-	cidade string
-}
+import (
+	"fmt"
+	"golang-do-zero-ao-avancado/struct/models"
+)
 
 func main() {
 	fmt.Println("Iniciando...")
 
-	endereco := endereco{
-		rua:    "Rua X",
-		numero: 200,
-		cidade: "Santa Cruz do Sul",
+	endereco := models.Endereco{
+		Rua:    "Rua X",
+		Numero: 200,
+		Cidade: "Santa Cruz do Sul",
 	}
 
-	fmt.Println(endereco)
-
-	endereco.numero = 150
+	endereco.Numero = 150
 
 	fmt.Println(endereco)
 
---
+++ struct/models/endereco.go
@@ -0,0 +1,7 @@
+package models
+
+type Endereco struct {
+	Rua    string
+	Numero int
+	Cidade string
+}
