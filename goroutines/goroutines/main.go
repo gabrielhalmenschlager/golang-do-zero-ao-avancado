e5a3778625afeb77a4bc8af0862765d1b5be810f
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-18 13:01:37 -0300

    Goroutines

---
+++ goroutines/goroutines/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+
+	for i := 0; i < 1000; i++ {
+		go showMessage(strconv.Itoa(i))
+	}
+
+	time.Sleep(time.Duration(time.Hour.Seconds() * float64(5)))
+}
+
+func showMessage(message string) {
+	fmt.Println(message)
+}
